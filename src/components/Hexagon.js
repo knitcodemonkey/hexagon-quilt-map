e6d66e7c80073237ef885cf6aaabbca6b8bc2e8c
Jen Luker <jen@gremlin.com>
2019-10-28 13:27:41 -0600

    Replace multiple divs with clip-path

--- src/components/Hexagon.js
+++ src/components/Hexagon.js
@@ -1,63 +1,49 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { relative } from "path";
 
 const Hexagon = ({ image, idx, fabric, debug }) => {
   return (
     <div
       css={{
+        position: "relative",
+        clipPath: "polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)",
         overflow: "hidden",
-        visibility: "hidden",
-        transform: "rotate(120deg)",
-        width: 225,
-        height: 100,
-        margin: `0 0 0 -55px`,
+        width: 115,
+        height: 102,
+        margin: `0 0 0 -27px`,
+        backgroundRepeat: "repeat",
+        backgroundImage: `url(./images/${fabric}/${fabric}${image}.jpg)`,
+        backgroundPosition: "center",
+
         ":nth-child(even)": {
           marginTop: 50,
         },
       }}
     >
-      <div
-        css={{
-          position: "relative",
-          overflow: "hidden",
-          width: "100%",
-          height: "100%",
-          transform: "rotate(-60deg)",
-          backgroundColor: "transparent",
-        }}
-      >
+      {debug === true && (
         <div
           css={{
             width: "100%",
             height: "100%",
-            backgroundRepeat: "repeat",
-            backgroundImage: `url(./images/${fabric}/${fabric}${image}.jpg)`,
-            backgroundPosition: "center",
-            visibility: "visible",
-            transform: "rotate(-60deg)",
-            backgroundOrigin: "border-box",
-            backgroundColor: "transparent",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          {debug === true && (
-            <div
-              css={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "flex-end",
-                zIndex: 10,
-                color: "blue",
-                fontSize: 24,
-                fontWeight: "bold",
-                textAlign: "center",
-                backgroundColor: "rgba(255, 255, 255, 0.7)",
-              }}
-            >
-              {`${image}`}
-            </div>
-          )}
+          <div
+            css={{
+              width: "100%",
+              zIndex: 10,
+              color: "blue",
+              fontSize: 24,
+              fontWeight: "bold",
+              textAlign: "center",
+              backgroundColor: "rgba(255, 255, 255, 0.6)",
+            }}
+          >{`${image}`}</div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
